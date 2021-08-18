0651b1ef8d49c88b763a3f7cbad2e18f16d3422c
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-18 21:43:09 +0900

    Add files via upload

--- src/main/java/com/example/tutorial/Application.java
+++ src/main/java/com/example/tutorial/Application.java
@@ -1,21 +1,11 @@
-package com.staxrt.tutorial;
+package com.example.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The type Application.
- *
- * @author Givantha Kalansuriya
- */
 @SpringBootApplication
 public class Application {
 
-  /**
-   * The entry point of application.
-   *
-   * @param args the input arguments
-   */
   public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
