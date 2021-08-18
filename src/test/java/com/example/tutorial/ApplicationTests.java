15f30e412517f193b1e76cd70a85123b33faa788
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-18 21:50:26 +0900

    Add files via upload

--- src/test/java/com/example/tutorial/ApplicationTests.java
+++ src/test/java/com/example/tutorial/ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.tutorial;
 
-import com.staxrt.tutorial.model.User;
+import com.example.tutorial.model.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
