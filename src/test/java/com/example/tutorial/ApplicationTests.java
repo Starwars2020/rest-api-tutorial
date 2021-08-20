57c9fc6c172f7ec6b750b946522637f6912e199a
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-20 21:50:56 +0900

    Add files via upload

--- src/test/java/com/example/tutorial/ApplicationTests.java
+++ src/test/java/com/example/tutorial/ApplicationTests.java
@@ -1,14 +1,18 @@
 package com.example.tutorial;
 
-import com.example.tutorial.model.User;
+import com.staxrt.tutorial.model.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.web.server.LocalServerPort;
-//import org.springframework.http.*;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
