9df5794e0e89f0c4a7cfec9b15606d1db2459a11
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-20 21:47:04 +0900

    Add files via upload

--- src/test/java/com/example/tutorial/ApplicationTests.java
+++ src/test/java/com/example/tutorial/ApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.jupiter.api.Test;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
 	@Autowired
