c0673aa97c702139c5d8238d8b3b00c2c03fafce
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-21 07:42:16 +0900

    Add files via upload

--- src/test/java/com/example/tutorial/ApplicationTests.java
+++ src/test/java/com/example/tutorial/ApplicationTests.java
@@ -1,31 +1,88 @@
 package com.example.tutorial;
 
 import com.example.tutorial.model.User;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
 
 	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-		this.mockMvc.perform(get("/api/user")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value(""));
+	public void contextLoads() {
 	}
+
+	@Test
+	public void testGetAllUsers() {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/users",
+				HttpMethod.GET, entity, String.class);
+
+		Assert.assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testGetUserById() {
+		User user = restTemplate.getForObject(getRootUrl() + "/users/1", User.class);
+		System.out.println(user.getName());
+		Assert.assertNotNull(user);
+	}
+
+	//@Test
+	//public void testCreateUser() {
+	//	User user = new User();
+	//	user.setName("admin");
+  //
+	//	ResponseEntity<User> postResponse = restTemplate.postForEntity(getRootUrl() + "/users", user, User.class);
+	//	Assert.assertNotNull(postResponse);
+	//	Assert.assertNotNull(postResponse.getBody());
+	//}
+
+	//@Test
+	//public void testUpdatePost() {
+	//	int id = 1;
+	//	User user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+	//	user.setName("admin1");
+  //
+	//	restTemplate.put(getRootUrl() + "/users/" + id, user);
+  //
+	//	User updatedUser = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+	//	Assert.assertNotNull(updatedUser);
+	//}
+
+	//@Test
+	//public void testDeletePost() {
+	//	int id = 2;
+	//	User user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+	//	Assert.assertNotNull(user);
+  //
+	//	restTemplate.delete(getRootUrl() + "/users/" + id);
+  //
+	//	try {
+	//		user = restTemplate.getForObject(getRootUrl() + "/users/" + id, User.class);
+	//	} catch (final HttpClientErrorException e) {
+	//		Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+	//	}
+	//}
 
 }
