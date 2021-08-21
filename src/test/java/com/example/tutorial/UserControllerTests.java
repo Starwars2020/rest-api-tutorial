3ae8a183cde7dcd559e0fbb194f52af7dc45d244
Starwars2020 <45616259+Starwars2020@users.noreply.github.com>
2021-08-21 18:12:39 +0900

    Add files via upload

--- src/test/java/com/example/tutorial/UserControllerTests.java
+++ src/test/java/com/example/tutorial/UserControllerTests.java
@@ -1,37 +1,42 @@
 package com.example.tutorial;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
 public class UserControllerTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc mvc;
 
-	//@Test
-	//public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+  @Test
+  public void user_to_return() throws Exception {
+    String user = "demo";
+  
+    mvc.perform(get("/demo"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(user));
+  }
+  
+  //@Test
+  //public void userAll_to_return() throws Exception {
+  //  String name = "demo";
+  //  int amount = 1000;
   //
-	//	this.mockMvc.perform(get("/demo/all")).andDo(print()).andExpect(status().isOk())
-	//			.andExpect(jsonPath("$.content").value(""));
-	//}
-
-	//@Test
-	//public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-  //
-	//	this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-	//			.andDo(print()).andExpect(status().isOk())
-	//			.andExpect(jsonPath("$.content").value("Hello, Spring Community"));
-	//}
-
+  //  mvc.perform(get("/demo/all")
+  //  		.param("name", name)
+  //  		.param("amount", String.valueOf(amount)))
+  //          .andExpect(status().isOk())
+  //          .andExpect(jsonPath("$.name", is(name)))
+  //          .andExpect(jsonPath("$.amount", is(amount)));
+  //}
 }
